ef671faf5a01b8254064d5901050d9b05d85fd51
annomy2 <annomy634@gmail.com>
2026-01-14 12:08:40 +0545

    top bar icon changed and added Input manager

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,17 +2,8 @@ extends  Node
 
 
 const resolution:Vector2 = Vector2(1280,720) * 2
-#
-#
-#func start_game():
-	#RelationManager.parse_geolocation_data()
-	#RelationManager.make_country_navigatable(PlayerData.country_id)
-#
-#
-#
-#func check_and_remove_exisiting_popups():
-	#if country_action_popups != null:
-		#RelationManager.highlight_country(country_action_popups.name,false)
-		#country_action_popups.name  = "removing..."
-		#country_action_popups.queue_free()
-		#country_action_popups = null
+enum InputModes {None,Placement,}
+var currentMode:InputModes = InputModes.None
+
+func _building_placement_mode():
+	pass
